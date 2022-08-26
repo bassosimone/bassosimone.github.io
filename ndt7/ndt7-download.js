b27fe5bb753c123402792b7fa23289a76762b019
Simone Basso <bassosimone@gmail.com>
2022-08-26 19:09:25 +0200

    cleanup: remove now-legacy ndt7+ implementations
    Development now happens elsewhere and no-one should keep using
    this code which at this point is obsolete.

--- ndt7/ndt7-download.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 6, asi: true, worker: true */
-// WebWorker that runs the ndt7 download test
-importScripts("ndt7-core.js")
-onmessage = function (ev) {
-  ndt7core.startDownload({
-    baseURL: ev.data.baseURL,
-    postMessage: self.postMessage.bind(self),
-  })
-}
--- ndt7/ndt7-upload.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 6, asi: true, worker: true */
-// WebWorker that runs the ndt7 upload test
-importScripts("ndt7-core.js")
-onmessage = function (ev) {
-  ndt7core.startUpload({
-    baseURL: ev.data.baseURL,
-    postMessage: self.postMessage.bind(self),
-  })
-}
